58cd84898c3df43d0dc1ca575de295eabc37b3a8
Patel-Meharsh <meharsh.patel@thegatewaycorp.co.in>
2026-09-23 18:40:39 +0530

    Centralize security capability definitions

---
+++ src/security/security-constants.js
@@ -0,0 +1,28 @@
+export const CAPABILITIES = Object.freeze([
+  "generalAI",
+  "webResearch",
+  "codeExecution",
+  "memory",
+  "imageUnderstanding",
+  "deviceIdentity",
+  "filesystemRead",
+  "filesystemWrite",
+  "filesystemDelete",
+  "terminal",
+  "applications",
+  "keyboard",
+  "mouse",
+  "remoteControl",
+  "admin",
+]);
+
+export const RESTRICTED_CAPABILITIES = Object.freeze([
+  "filesystemWrite",
+  "filesystemDelete",
+  "terminal",
+  "applications",
+  "keyboard",
+  "mouse",
+  "remoteControl",
+  "admin",
+]);
